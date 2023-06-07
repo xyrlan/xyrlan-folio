1fed018ef198620aee96a27f13469b2571dfa370
xyrlan; pedro <lobatopedro68@gmail.com>
2023-06-07 14:23:27 -0300

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
     return (
         <nav className={`sticky top-0 bg-slate-100 flex w-full lg:p-5 lg:px-10 max-lg:pr-5 justify-between z-50 ${!isActive ? '' : ''}`}>
             <Link href="/" className="flex hover:scale-95 duration-500 w-fit items-center">
-                <motion.img initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 0.2 }} className=" duration-500 z-20 cursor-pointer" src={'/images/flor-da-vida.svg'} width={120} height={84} />
+                <motion.img initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.4 }} className=" duration-500 z-20 cursor-pointer" src={'/images/flor-da-vida.svg'} width={120} height={84} />
                 <p className="text-xl font-extrabold z-20">&quot;xyrlan&quot;</p>
             </Link>
 
