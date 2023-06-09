3d1ec64679e8604655575e5c04c499a362b41f9c
Xyrlan <lobatopedro8@gmail.com>
2023-06-09 09:42:13 -0300

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
 
     return (
         <nav className={`sticky top-0 bg-slate-100 flex w-full lg:p-5 lg:px-10 max-lg:pr-5 justify-between z-50 ${!isActive ? '' : ''}`}>
-            <Link href="/" className="flex hover:scale-95 duration-500 w-fit items-center">
+            <Link href="/" className="flex hover:scale-95 duration-500 w-fit items-center z-40">
                 <motion.img initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.4 }} className=" duration-500 z-20 cursor-pointer" src={'/images/flor-da-vida.svg'} width={120} height={84} />
                 <p className="text-xl font-extrabold z-20">&quot;xyrlan&quot;</p>
             </Link>
@@ -30,7 +30,7 @@ export default function Navbar() {
                 className={`sm:hidden block max-sm:mx-4 z-50 ${!isActive ? '' : ''} `} id="menu-icon" src="/images/menu-icon.svg" />
 
             {isActive && (
-                <ul className="fixed bg-slate-100 h-full w-full flex flex-col justify-center items-center gap-10">
+                <ul className="fixed bg-slate-100 h-full w-full flex flex-col justify-center items-center gap-10 z-30">
                     <motion.li initial={{ y: 300, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ duration: 0.4 }}>
                         <Link href="/" 
                          onClick={() => {setActive(false)}} 
