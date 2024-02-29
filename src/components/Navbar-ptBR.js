42383bc572c37d08c24a518de20b7fc1e236e04e
xyrlan <lobatopedro68@gmail.com>
2024-02-29 15:28:11 -0400

    Fix Navbar language switcher styling

--- src/components/Navbar-ptBR.js
+++ src/components/Navbar-ptBR.js
@@ -17,10 +17,10 @@ export default function NavbarptBR({ setLanguage }) {
 
     return (
         <nav className={`sticky top-0 bg-slate-100 flex w-full lg:p-5 lg:px-10  justify-center border-b-2 z-50 overflow-hidden ${!isActive ? '' : ''}`}>
-            <div className="container flex w-full lg:p-5 lg:px-10 justify-between z-50 ">
+            <div className="container flex w-full lg:p-5 lg:px-10 justify-between  ">
                 <Link href="/" className="flex hover:scale-95 duration-500 w-fit items-center z-40">
                     <motion.img initial={{ x: -100 }} animate={{ x: 0 }} transition={{ duration: 0.2 }} whileTap={{ scale: 0.4 }} className=" duration-500 z-20 cursor-pointer" src={'/images/flor-da-vida.svg'} width={120} height={84} />
-                    <p className="text-xl font-extrabold z-20">&quot;xyrlan&quot;</p>
+                    <p className="text-xl font-extrabold z-20">xyrlan</p>
                 </Link>
 
 
@@ -37,7 +37,7 @@ export default function NavbarptBR({ setLanguage }) {
                     className={`h-auto w-12 sm:hidden max-sm:mx-4 z-50 duration-200 transition-all overflow-hidden ${isActive ? 'opacity-100' : 'opacity-0 translate-x-14 right-0 top-4 absolute'} `} />
 
                 {isActive && (
-                    <motion.ul initial={{ height: 0 }} whileInView={{ height: '100%', opacity: 1 }} transition={{ duration: 1 }} className={`fixed bg-slate-100 w-full flex flex-col justify-start pt-32 items-center gap-10 z-30 top-0`}>
+                    <motion.ul initial={{ height: 0 }} whileInView={{ height: '100%', opacity: 1 }} transition={{ duration: 1 }} className={`fixed bg-slate-100 w-full h-full flex flex-col justify-start pt-32 items-center gap-10 z-30 top-0`}>
                         <motion.li initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.4 }}>
                             <Link href="/ptBR" onClick={() => { setActive(false) }} className="text-5xl font-bold cursor-pointer hover:border-b-4 hover:text- hover:border-black duration-200">Início</Link>
                         </motion.li>
