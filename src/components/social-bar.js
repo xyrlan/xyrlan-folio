34023fc8f04301a779e345fa24fa3b8da37b00fe
xyrlan <lobatopedro68@gmail.com>
2024-04-15 16:46:53 -0400

    Update LinkedIn profile link in social-bar.js

--- src/components/social-bar.js
+++ src/components/social-bar.js
@@ -12,7 +12,7 @@ export default function SocialBar() {
                     <a target='_blank' href="https://www.frontendmentor.io/profile/xyrlan"><motion.img initial={{ opacity: 0, scale: 0 }} transition={{ duration: 0.2 }} whileInView={{ opacity: 1, scale: 1 }} whileTap={{ scale: 0.8 }} className=' h-8 cursor-pointer' src='/images/frontend-icon.svg' /></a>
                 </li>
                 <li>
-                   <a target='_blank' href="https://www.linkedin.com/in/pedro-lobato-77b23021a/"><motion.img initial={{ opacity: 0, scale: 0 }} transition={{ duration: 0.2 }} whileInView={{ opacity: 1, scale: 1 }} whileTap={{ scale: 0.8 }} className=' h-8 cursor-pointer' src='/images/linkedin-icon.svg' /></a> 
+                   <a target='_blank' href="https://www.linkedin.com/in/pedro-lobato-xyrlan/"><motion.img initial={{ opacity: 0, scale: 0 }} transition={{ duration: 0.2 }} whileInView={{ opacity: 1, scale: 1 }} whileTap={{ scale: 0.8 }} className=' h-8 cursor-pointer' src='/images/linkedin-icon.svg' /></a> 
                 </li>
                 <li>
                    <a target='_blank' href="https://api.whatsapp.com/send/?phone=5561999936169"><motion.img initial={{ opacity: 0, scale: 0 }} transition={{ duration: 0.2 }} whileInView={{ opacity: 1, scale: 1 }} whileTap={{ scale: 0.8 }} className=' h-8 cursor-pointer' 
